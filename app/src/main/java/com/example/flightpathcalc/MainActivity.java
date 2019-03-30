d01ff564745af4f4b8ce843ee9a8371835b47820
hubertndahura <hubertndahura12@gmail.com>
2019-03-30 22:16:10 +0300

    adding codes to move from the splash activity to the home activity

--- app/src/main/java/com/example/flightpathcalc/MainActivity.java
+++ app/src/main/java/com/example/flightpathcalc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.flightpathcalc;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    Timer timer;
 
     private DrawerLayout nDrawerLayout;
     private ActionBarDrawerToggle nToggle;
@@ -16,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Newactivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
 
         try {
             nDrawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
