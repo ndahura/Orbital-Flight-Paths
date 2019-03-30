4b39f3b89b61db40a510595204635a6387d1ccf7
Stephen Odundo <steveremdi@gmail.com>
2019-03-30 21:54:26 +0300

    Fixed crash-at-launch bug

--- app/src/main/java/com/example/flightpathcalc/MainActivity.java
+++ app/src/main/java/com/example/flightpathcalc/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nDrawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
-        nToggle=new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
-        nDrawerLayout.addDrawerListener(nToggle);
-        nToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        try {
+            nDrawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
+            nToggle=new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
+            nDrawerLayout.addDrawerListener(nToggle);
+            nToggle.syncState();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        catch (Exception ex)
+        {
+            Log.d("MainActivity --> ", "onCreate: " + ex);
+        }
+
+
     }
 
     @Override
